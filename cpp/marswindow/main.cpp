536a76a2bda7da764e61758487212fd4e818d152
george <123@321.com>
2018-04-23 23:26:39 +0300

    marswindow problem

---
+++ cpp/marswindow/main.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+int main() {
+  int given_year; // [2018, 10000]
+  std::scanf("%d", &given_year);
+
+  if (given_year == 2018) {
+    std::printf("yes\n");
+    return 0;
+  }
+
+  const int launch_year = 2018; // optimal launch year
+  const int launch_month = 3;   // optimal launch month
+  const int launch_cycle = 26;  // optimal launch occurs every 26 months
+
+  // number of months from Apr 2018(not from Jan 2018, that's why
+  // we substract 3) to Jan given_year
+  int months_diff = (given_year - launch_year) * 12 - launch_month;
+
+  // months_diff will always be not divisible by 26
+  int cycles = months_diff / launch_cycle + 1;
+
+  // months to add to Jan given_year
+  int months_to_add = cycles * launch_cycle - months_diff;
+
+  if (months_to_add < 12)
+    std::printf("yes\n");
+  else
+    std::printf("no\n");
+
+  return 0;
+}
